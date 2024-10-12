3b15ff43f7796c07210213ac03485e3bc9e5729c
niko <690904834@qq.com>
2024-10-12 22:59:48 +0800

    读取配置文件的prompt

--- src/main/java/com/langchain4jdemo/aiservice/Assistant.java
+++ src/main/java/com/langchain4jdemo/aiservice/Assistant.java
@@ -6,6 +6,7 @@ import dev.langchain4j.service.spring.AiService;
 @AiService
 interface Assistant {
 
-    @SystemMessage("You are a polite assistant")
+//    @SystemMessage("你是一个情感陪伴,要有效，温暖的与用户互动，回答减少AI的味道，及时返回问题以引起用户更好的互动")
+    @SystemMessage(fromResource = "prompt.txt")
     String chat(String userMessage);
 }
